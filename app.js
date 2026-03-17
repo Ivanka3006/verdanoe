9225083628a708c6505715dbe338043acb2f1083
Ivanka3006 <bilskaivanka@gmail.com>
2026-03-17 10:05:24 +0200

    app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const plants = [
-  { name: "Монстера", price: 300 },
-  { name: "Фікус", price: 250 },
-  { name: "Сукулент", price: 150 },
-  { name: "Кактус", price: 120 }
+  { name: "Троянда", price: 100, category: "Квіти" },
+  { name: "Кактус", price: 50, category: "Сукуленти" }
 ];
 
-const catalog = document.getElementById("catalog");
+function render() {
+  const catalog = document.getElementById("catalog");
+  catalog.innerHTML = "";
 
-plants.forEach(p => {
-  const div = document.createElement("div");
-  div.className = "card";
-  div.innerHTML = <h3>${p.name}</h3><p>${p.price} грн</p>;
-  catalog.appendChild(div);
-});
+  plants.forEach(p => {
+    const div = document.createElement("div");
+    div.innerHTML = <h3>${p.name}</h3><p>${p.price} грн</p>;
+    catalog.appendChild(div);
+  });
+}
+
+render();
