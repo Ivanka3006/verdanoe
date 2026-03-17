6f5f73ec21b4a216b8c9e34b293b71d779f335ac
Ivanka3006 <bilskaivanka@gmail.com>
2026-03-17 13:48:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,28 @@ const cartBox = document.getElementById("cart-box");
 const cartCount = document.getElementById("cart-count");
 
 let plants = [
-  { id: 1, name: "Туя Смарагд", price: 250, category: "хвойні" },
-  { id: 2, name: "Сосна", price: 300, category: "хвойні" },
-  { id: 3, name: "Яблуня", price: 200, category: "дерева" },
-  { id: 4, name: "Троянда", price: 150, category: "квіти" },
-  { id: 5, name: "Петунія", price: 80, category: "однорічні" },
-  { id: 6, name: "Лаванда", price: 120, category: "багаторічні" },
-  { id: 7, name: "Алоє", price: 90, category: "сукуленти" }
-];
+  {
+    id: 1,
+    name: "Туя Смарагд",
+    price: 250,
+    category: "хвойні",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Thuja_occidentalis_Smaragd.jpg/320px-Thuja_occidentalis_Smaragd.jpg"
+  },
+  {
+    id: 2,
+    name: "Троянда",
+    price: 150,
+    category: "квіти",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Rose_pink_1.jpg/320px-Rose_pink_1.jpg"
+  },
+  {
+    id: 3,
+    name: "Яблуня",
+    price: 200,
+    category: "дерева",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Apple_tree.jpg/320px-Apple_tree.jpg"
+  }
+];;
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 let currentCategory = "всі";
