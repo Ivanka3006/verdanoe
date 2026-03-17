60ff050861f31d24e1f4faad172a52666805a4f1
Ivanka3006 <bilskaivanka@gmail.com>
2026-03-17 11:25:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const catalog = document.getElementById("catalog");
 
+// беремо дані з localStorage
 const plant = JSON.parse(localStorage.getItem("plant"));
 
 if (plant) {
   catalog.innerHTML = `
-    <h3>${plant.name}</h3>
-    <p>${plant.desc}</p>
+    <div>
+      <h3>${plant.name}</h3>
+      <p>${plant.desc}</p>
+    </div>
   `;
+} else {
+  catalog.innerHTML = "<p>Немає рослин</p>";
 }
