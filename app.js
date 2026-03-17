8de68d911578d8a7436fcf071ee6b59b3159e23f
Ivanka3006 <bilskaivanka@gmail.com>
2026-03-17 09:58:45 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const plants = [
+  { name: "Монстера", price: 300 },
+  { name: "Фікус", price: 250 },
+  { name: "Сукулент", price: 150 }
+];
+
+const catalog = document.getElementById("catalog");
+
+plants.forEach(p => {
+  const div = document.createElement("div");
+  div.className = "card";
+  div.innerHTML = <h3>${p.name}</h3><p>${p.price} грн</p>;
+  catalog.appendChild(div);
+});
