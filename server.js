30d8afdb7bf497f5a33d4a419e0f479af6469863
Ivanka3006 <bilskaivanka@gmail.com>
2026-03-17 10:30:14 +0200

    Update and rename server.js to

--- server.js
+++ server.js
@@ -1,17 +1,1 @@
-const plants = [
-  { name: "Троянда", price: 100, category: "Квіти" },
-  { name: "Кактус", price: 50, category: "Сукуленти" }
-];
 
-function render() {
-  const catalog = document.getElementById("catalog");
-  catalog.innerHTML = "";
-
-  plants.forEach(p => {
-    const div = document.createElement("div");
-    div.innerHTML = <h3>${p.name}</h3><p>${p.price} грн</p>;
-    catalog.appendChild(div);
-  });
-}
-
-render();
