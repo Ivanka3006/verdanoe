59edd4236bb19905e30719ba1b89f68dcc1ae120
Ivanka3006 <bilskaivanka@gmail.com>
2026-03-17 11:08:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const catalog = document.getElementById("catalog");
+
+catalog.innerHTML = `
+  <div>
+    <h3>🌿 Монстера</h3>
+    <p>Красива кімнатна рослина</p>
+  </div>
+
+  <div>
+    <h3>🌵 Кактус</h3>
+    <p>Невибаглива рослина</p>
+  </div>
+`;
